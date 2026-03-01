9e68e14c117238d9d2c787599c0d269f9f8150d0
Kraftifyweb <kraftifyweb@gmail.com>
2026-03-01 14:33:29 +0530

    logo commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="50px"/>
+        hi
       </header>
     </div>
   );

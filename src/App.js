79195abbbc7df9f6b6d9e962ae510020f09cb129
Kraftifyweb <kraftifyweb@gmail.com>
2026-03-03 09:12:19 +0530

    favicon responsive  nav

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width="50px"/>
         hi
-        how are you
+        how are you im fine
       </header>
       <Foot />
     </div>

9aff23e945b0102359e52391090aa929d180cd7e
Kraftifyweb <kraftifyweb@gmail.com>
2026-03-05 07:19:08 +0530

    movile

--- src/comp/Home/tag/Tag.jsx
+++ src/comp/Home/tag/Tag.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 function Tag() {
   return (
     <div>
-        hi
+        here we share all news sports related contents 
+        <h1>News </h1><br></br>
+        <p>
+            <h3>
+                T20 World Cup: Focus on match-ups as Proteas meet Black Caps for a spot in the final
+            </h3>
+            <br>
+            </br>
+            <h4>A lot will depend on how the South African batters fare against the Kiwi spinners; Markram’s men are the only unbeaten side left in the competition; New Zealand sweats over lead pacer Henry’s fitness</h4>
+        </p>
+        <h5>
+            Semifinals distil tournaments to their essence. At Eden Gardens, they also stir memory. South Africa returns to Kolkata for the first semifinal of the ICC T20 World Cup against New Zealand with proof that this ground can bend to visiting will. Last November, the Proteas won a low-scoring Test here by 30 runs, their first in India since 2010. Useful memory, perhaps. Protection, none. T20 offers no shelter.
+            <br></br>
+            Rachin Ravindra’s left-arm spin suited Colombo’s slow turners. Kolkata is unlikely to offer grip as readily. South Africa’s top seven has three left-handers: Quinton de Kock, Ryan Rickelton and David Miller. Since mid-2024, they have scored at better than eight an over against left-arm spin in overs seven to 15. So, Ravindra may be a side act here. Captain Mitchell Santner’s angles and subtle changes of pace feel likelier to matter.
+            <br></br><br></br>
+            he symmetry cuts both ways. A left-heavy batting order invites off-spin. Glenn Phillips could be used early against de Kock, vulnerable in that match-up. Hold Phillips back, though, and he risks confronting the power of right-handers Aiden Markram, Dewald Brevis, Tristan Stubbs and Marco Jansen.
+<br></br><br></br>
+The Kiwis have a selection dilemma too. Persist with Cole McConchie’s off-spin, or add seam through Jimmy Neesham/Jacob Duffy, especially with uncertainty over whether Matt Henry will play? The call may hinge on how New Zealand believes the surface will behave under lights.
+<br></br><br></br>
+The closing overs will be decisive. New Zealand has conceded 9.52 per over between overs 15 and 20. South Africa’s boundary percentage in that phase is a modest 16.16, but its dot-ball share under 24, the lowest of all teams, points to a side comfortable accumulating when spectacle fails.
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+Both teams understand the conditions, having played on varying surfaces in the group stage and Super Eight. The semifinal will turn on who manages them when the margins shrink.
+        </h5><br></br>
     </div>
   )
 }
